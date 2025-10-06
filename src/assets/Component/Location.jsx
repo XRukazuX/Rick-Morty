cc9e71b45cdfa9232ab0f468329f5d70cc716d49
Lucas <lucas_mamani_12@hotmail.com>
2025-10-06 09:26:43 -0300

    Obtener datos de los personajes del episodio

--- src/assets/Component/Location.jsx
+++ src/assets/Component/Location.jsx
@@ -1,22 +1,37 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 function Location() {
-  const { id } = useParams();
-  const [Location, setLocation] = useState(null);
-  const character = Location?.residents
-    ?.map((e) => e.split("/").pop())
-    .join(",");
+  const { id } = useParams(); //id o codigo de localizacion a ver
+  const [Location, setLocation] = useState(null); //Datos de la localiacion pedida
+  const [Characters, setCharacters] = useState(null); //Datos del personaje
+  const character = useMemo(() => {
+    return Location?.residents?.map((e) => e.split("/").pop()).join(","); //Contiene los id de los caracteres que ocupan la localizacion;
+  }, [Location]);
+  const api = useMemo(() => {
+    return `https://rickandmortyapi.com/api/character/${character}`;
+  }, [character]);
   const apiLocation = (a) => {
     fetch(`https://rickandmortyapi.com/api/location/${a}`)
       .then((e) => e.json())
       .then((k) => setLocation(k))
       .catch((error) => console.log("Error", error));
+  }; //Funcion para obtener la informacion para "Location"
+  const apiCharacters = (e) => {
+    fetch(e)
+      .then((e) => e.json())
+      .then((e) => setCharacters(e))
+      .catch((error) => console.log("Error de character", error));
   };
   useEffect(() => {
     apiLocation(id);
-  }, []);
+  }, []); //Solo que se obtengan los datos al cargar la pagina
+  useEffect(() => {
+    if (Location) {
+      apiCharacters(api);
+    }
+  }, [Location]); //Se obtiene datos de los personajes del episodio solo cuando tengo Location
   console.log(Location);
-  console.log(character);
+  console.log(Characters);
   return (
     <>
       <h3>Entro a location</h3>
